88acc75874017f2bf689f71150f9cd7e2cb46c30
deep.gajera <deep.gajera@crestdatasys.com>
2021-05-03 12:20:29 +0530

    add cc_service_backends

---
+++ illumio-core/data_source_illumio_container_cluster_service_backends.go
@@ -0,0 +1,114 @@
+package illumiocore
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func datasourceIllumioContainerClusterServiceBackends() *schema.Resource {
+	return &schema.Resource{
+		ReadContext:   datasourceIllumioContainerClusterServiceBackendsRead,
+		SchemaVersion: version,
+		Description:   "Represents Illumio Container Cluster Service Backend",
+
+		Schema: map[string]*schema.Schema{
+			"href": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "URI of Container Cluster Service backend",
+			},
+			"items": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "Collection of Container Cluster Service Backends",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name of the Container Cluster Backend",
+						},
+						"kind": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The type (or kind) of Container Cluster Backend",
+						},
+						"namespace": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The namespace of the Container Cluster Backend",
+						},
+						"updated_at": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The time (rfc339 timestamp) at which the Container Cluster Backend was last updated",
+						},
+						"created_at": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The time (rfc339 timestamp) in which the Container Cluster Backend was created",
+						},
+						"virtual_service": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "Associated virtual service. Single element list",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"href": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The URI to the associated virtual service",
+									},
+									"name": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The name of virtual service",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func datasourceIllumioContainerClusterServiceBackendsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diagnostics diag.Diagnostics
+	pConfig, _ := m.(Config)
+	illumioClient := pConfig.IllumioClient
+
+	href := d.Get("href").(string)
+
+	_, data, err := illumioClient.Get(href, nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(href)
+
+	sbs := []map[string]interface{}{}
+	for _, child := range data.Children() {
+		sbsKeys := []string{
+			"name",
+			"kind",
+			"namespace",
+			"updated_at",
+			"created_at",
+		}
+		sb := gabsToMap(child, sbsKeys)
+
+		if child.Exists("virtual_service") {
+			sb["virtual_service"] = []interface{}{
+				gabsToMap(child.S("virtual_service"), []string{"href", "name"}),
+			}
+		}
+		sbs = append(sbs, sb)
+	}
+	d.Set("items", sbs)
+
+	return diagnostics
+}
---
+++ website/docs/d/container_cluster_service_backends.html.md
@@ -0,0 +1,51 @@
+---
+page_title: "illumio-core_container_cluster_service_backends Data Source - terraform-provider-illumio-core"
+subcategory: ""
+description: |-
+  Represents Illumio Container Cluster Service Backends
+---
+
+# illumio-core_container_cluster_service_backends (Data Source)
+
+Represents Illumio Container Cluster Service Backends
+
+Example Usage
+------------
+
+```hcl
+data "illumio-core_container_cluster_service_backends" "example" {
+  href = "/orgs/1/container_clusters/f959d2d0-fe56-4bd9-8132-b7a31d1cbdde/service_backends"
+}
+```
+
+## Schema
+
+### Required
+
+- **href** (String) URI of Container Cluster Service backends
+
+### Read-Only
+
+- **items** (List of Object) Collection of Container Cluster Service Backends (see [below for nested schema](#nestedatt--items))
+
+<a id="nestedatt--items"></a>
+### Nested Schema for `items`
+
+Read-Only:
+
+- **created_at** (String) The time (rfc339 timestamp) in which the Container Cluster Backend was created
+- **kind** (String) The type (or kind) of Container Cluster Backend
+- **name** (String) The name of the Container Cluster Backend
+- **namespace** (String) The namespace of the Container Cluster Backend
+- **updated_at** (String) The time (rfc339 timestamp) at which the Container Cluster Backend was last updated
+- **virtual_service** (List of Object) Associated virtual service. Single element list (see [below for nested schema](#nestedobjatt--items--virtual_service))
+
+<a id="nestedobjatt--items--virtual_service"></a>
+### Nested Schema for `items.virtual_service`
+
+Read-Only:
+
+- **href** (String) The URI to the associated virtual service
+- **name** (String) The name of virtual service
+
+

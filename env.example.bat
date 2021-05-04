368bd59840a17d1c794fbcb573d18cd2aa73782c
Kyong Kim <kyong.kim@illumio.com>
2021-05-03 17:35:56 -0700

    checking my key

--- env.example.bat
+++ env.example.bat
@@ -1,3 +1,3 @@
-set ILLUMIO_PCE_HOST=https://2x2devtest59.ilabs.io:8443
-set ILLUMIO_API_KEY_USERNAME=api_xxxxxxxxxxxxx
-set ILLUMIO_API_KEY_SECRET=yyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+export ILLUMIO_PCE_HOST=https://2x2devtest59.ilabs.io:8443
+export ILLUMIO_API_KEY_USERNAME=api_xxxxxxxxxxxxx
+export ILLUMIO_API_KEY_SECRET=yyyyyyyyyyyyyyyyyyyyyyyyyyyyy

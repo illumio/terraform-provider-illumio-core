cf59996ebee97332486b60f96a03a3cced510963
Deep Gajera <deep.gajera@crestdatasys.com>
2021-05-06 17:15:48 +0530

    Implements data sources {container_clusters, enforcement_boundaries, vulnerability_reports, workload_profiles}

---
+++ examples/enforcement_boundaries/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    illumio-core = {
+      version = "0.1"
+      source  = "illumio.com/labs/illumio-core"
+    }
+  }
+}
+
+
+provider "illumio-core" {
+  //  pce_host              = "https://pce.my-company.com:8443"
+  //  api_username          = "api_xxxxxx"
+  //  api_secret            = "big-secret"
+  request_timeout = 30
+  org_id          = 1
+}
+
+data "illumio-core_enforcement_boundaries" "example" {
+  max_results = 5
+  name = "testing eb"
+}

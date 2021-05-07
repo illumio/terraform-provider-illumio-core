6985c52c6b4a6e9ec1200646fbe17e8a0c49ddab
deep.gajera <deep.gajera@crestdatasys.com>
2021-05-07 11:05:57 +0530

    add more collection data sources
    rework rule set and security rule read functions.
    add new collection data sources:
    pairing_profiles,
    rule_sets
    security_rules,
     traffic_collector_settings_list

---
+++ examples/traffic_collector_settings_list/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    illumio-core = {
+      version = "0.1"
+      source  = "illumio.com/labs/illumio-core"
+    }
+  }
+}
+
+provider "illumio-core" {
+  //  pce_host              = "https://pce.my-company.com:8443"
+  //  api_username          = "api_xxxxxx"
+  //  api_secret            = "big-secret"
+  request_timeout = 30
+  org_id          = 1
+}
+
+data "illumio-core_traffic_collector_settings_list" "example" {}
---
+++ illumio-core/data_source_illumio_traffic_collector_settings_list.go
@@ -0,0 +1,141 @@
+package illumiocore
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+/* Sample API responce
+[
+	{
+		"href": "string",
+		"transmission": "string",
+		"target": {
+			"dst_port": 0,
+			"proto": 0,
+			"dst_ip": "string"
+		},
+		"action": "string"
+	}
+]
+*/
+
+func datasourceIllumioTrafficCollectorSettingsList() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: datasourceIllumioTrafficCollectorSettingsListRead,
+
+		SchemaVersion: version,
+		Description:   "Represents Illumio Traffic Collector Settings",
+
+		Schema: map[string]*schema.Schema{
+			"items": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "list of Traffic Collector Setting hrefs",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"href": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "URI of Traffic Collector Setting",
+						},
+						"transmission": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `transmission type`,
+						},
+						"action": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: `action for target traffic`,
+						},
+						"target": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "target for traffic collector settings",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"dst_port": {
+										Type:        schema.TypeInt,
+										Computed:    true,
+										Description: "destination port for target",
+									},
+									"proto": {
+										Type:        schema.TypeInt,
+										Computed:    true,
+										Description: "protocol for target",
+									},
+									"dst_ip": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: `single ip address or CIDR`,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func datasourceIllumioTrafficCollectorSettingsListRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	pConfig, _ := m.(Config)
+	illumioClient := pConfig.IllumioClient
+
+	href := fmt.Sprintf("/orgs/%v/settings/traffic_collector", pConfig.OrgID)
+
+	_, data, err := illumioClient.Get(href, nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(href)
+
+	tcsKeys := []string{
+		"href",
+		"action",
+	}
+
+	tcss := []map[string]interface{}{}
+
+	for _, trafficCS := range data.Children() {
+		tcs := gabsToMap(trafficCS, tcsKeys)
+
+		key := "transmission"
+		if trafficCS.Exists(key) {
+			switch trafficCS.S(key).Data().(string) {
+			case "B":
+				tcs[key] = "broadcast"
+			case "M":
+				tcs[key] = "multicast"
+			}
+		} else {
+			tcs[key] = nil
+		}
+
+		key = "target"
+		if trafficCS.Exists(key) {
+			targetKeys := []string{
+				"dst_port",
+				"proto",
+				"dst_ip",
+			}
+			tcs[key] = []interface{}{
+				gabsToMap(trafficCS.S(key), targetKeys),
+			}
+		} else {
+			tcs[key] = nil
+		}
+
+		tcss = append(tcss, tcs)
+	}
+
+	d.Set("items", tcss)
+
+	return diags
+}

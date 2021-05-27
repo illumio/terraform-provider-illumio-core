6a3f0c9acf91b17a7c4eed652a7cc9e4d08eb870
Deep Gajera <deep.gajera@crestdatasys.com>
2021-05-27 19:47:55 +0530

    gomod111 on

--- scripts/acccoveragecheck.sh
+++ scripts/acccoveragecheck.sh
@@ -2,14 +2,14 @@
 
 set -e -u
 
-go mod vendor
+# go mod vendor
 
 # TODO remove run flag once acc. tests job setup is done
 go test ./illumio-core -run="TestProvider" -coverprofile=cover.out
 
 perc=`go tool cover -func=cover.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+)?)%$!\1!'`
 res=${perc%.*}
-if [[ "$int" -gt "70" ]];
+if [[ "$res" -gt "70" ]];
   then
     echo "Coverage: $perc PASS"
   else

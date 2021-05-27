738a66038fe9a5a1d78ecdd85932062b979501bf
Deep Gajera <deep.gajera@crestdatasys.com>
2021-05-27 19:58:36 +0530

    using pkg_list

--- scripts/acccoveragecheck.sh
+++ scripts/acccoveragecheck.sh
@@ -3,9 +3,10 @@
 set -e -u
 
 # go mod vendor
+pkg_list=`go list ./... | grep -v /vendor/`
 
 # TODO remove run flag once acc. tests job setup is done
-go test ./illumio-core -run="TestProvider" -coverprofile=cover.out
+go test ./illumio-core -run="TestProvider" -coverprofile=cover.out $pkg_list
 
 perc=`go tool cover -func=cover.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+)?)%$!\1!'`
 res=${perc%.*}

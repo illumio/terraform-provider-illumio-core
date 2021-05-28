10aaca3dd05cb07d5768168bb1a97a0874ec55d5
Deep Gajera <deep.gajera@crestdatasys.com>
2021-05-28 10:55:37 +0530

    Using ge instead of gt

--- scripts/acccoveragecheck.sh
+++ scripts/acccoveragecheck.sh
@@ -9,7 +9,7 @@ go test ./illumio-core -run="TestProvider" -coverprofile=cover.out
 
 perc=`go tool cover -func=cover.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+)?)%$!\1!'`
 res=${perc%.*}
-if [[ "$res" -gt "70" ]];
+if [[ "$res" -ge "70" ]];
   then
     echo "Coverage: $perc PASS"
   else

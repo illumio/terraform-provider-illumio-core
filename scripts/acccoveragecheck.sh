40f70a9695fe0f0a9c72728bde8b0bd00c3d2941
Deep Gajera <deep.gajera@crestdatasys.com>
2021-06-02 15:06:04 +0530

    Adding configurable param coverage threshold

--- scripts/acccoveragecheck.sh
+++ scripts/acccoveragecheck.sh
@@ -9,7 +9,7 @@ go test ./illumio-core -run="TestProvider" -coverprofile=cover.out
 
 perc=`go tool cover -func=cover.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+)?)%$!\1!'`
 res=${perc%.*}
-if [[ "$res" -ge "70" ]];
+if [[ "$res" -ge "$ACC_TEST_COV_THRESHOLD" ]];
   then
     echo "Coverage: $perc PASS"
   else

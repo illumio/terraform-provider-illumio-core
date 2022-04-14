c9a41822b9a57207f4d2ae90e2056f8d3695ff22
Duncan Sommerville <duncan.sommerville@illumio.com>
2022-04-14 00:55:10 -0400

    fix service binding tests to work with updated virtual service functionality

--- illumio-core/data_source_illumio_service_binding_test.go
+++ illumio-core/data_source_illumio_service_binding_test.go
@@ -4,10 +4,12 @@ package illumiocore
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 var prefixSB string = "TF-ACC-SB"
@@ -16,12 +18,19 @@ func TestAccIllumioSB_Read(t *testing.T) {
 	dataSourceName := "data.illumio-core_service_binding.sb_test"
 	resourceName := "illumio-core_service_binding.sb_test"
 
+	virtualServiceName := acctest.RandomWithPrefix(prefixSB)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: TestAccProviderFactories,
+		CheckDestroy:      testAccCheckIllumioSBDataSource_VSDestroyed,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIllumioSBDataSourceConfig_basic(),
+				Config: testAccCheckIllumioSBDataSourceConfig_VSSetup(virtualServiceName),
+				Check:  testAccCheckIllumioSBDataSource_VSExists("illumio-core_virtual_service.test"),
+			},
+			{
+				Config: testAccCheckIllumioSBDataSourceConfig_basic(virtualServiceName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair(dataSourceName, "href", resourceName, "href"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "virtual_service", resourceName, "virtual_service"),
@@ -32,12 +41,9 @@ func TestAccIllumioSB_Read(t *testing.T) {
 	})
 }
 
-func testAccCheckIllumioSBDataSourceConfig_basic() string {
-	rName1 := acctest.RandomWithPrefix(prefixSB)
-	rName2 := acctest.RandomWithPrefix(prefixSB)
-
+func testAccCheckIllumioSBDataSourceConfig_VSSetup(name string) string {
 	return fmt.Sprintf(`
-resource "illumio-core_virtual_service" "sb_test" {
+resource "illumio-core_virtual_service" "test" {
 	name = %[1]q
 	description = "Terraform Service Binding test"
 	apply_to = "host_only"
@@ -55,16 +61,25 @@ resource "illumio-core_virtual_service" "sb_test" {
 		"1.2.3.4"
 	]
 }
+`, name)
+}
 
+func testAccCheckIllumioSBDataSourceConfig_basic(virtualServiceName string) string {
+	rName := acctest.RandomWithPrefix(prefixSB)
+
+	var sb strings.Builder
+
+	sb.WriteString(testAccCheckIllumioSBDataSourceConfig_VSSetup(virtualServiceName))
+	sb.WriteString(fmt.Sprintf(`
 resource "illumio-core_unmanaged_workload" "sb_test" {
-	name               = %[2]q
+	name               = %[1]q
 	description        = "Terraform Service Binding test"
 	hostname           = "example.workload"
 }
 
 resource "illumio-core_service_binding" "sb_test" {
 	virtual_service {
-		href = illumio-core_virtual_service.sb_test.href
+		href = replace(illumio-core_virtual_service.test.href, "draft", "active")
 	}
 	workload {
 		href = illumio-core_unmanaged_workload.sb_test.href
@@ -74,5 +89,51 @@ resource "illumio-core_service_binding" "sb_test" {
 data "illumio-core_service_binding" "sb_test" {
 	href = illumio-core_service_binding.sb_test.href
 }
-`, rName1, rName2)
+`, rName))
+	return sb.String()
+}
+
+func testAccCheckIllumioSBDataSource_VSExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resourceName)
+		}
+
+		href := rs.Primary.ID
+
+		if href == "" {
+			return fmt.Errorf("Widget ID is not set")
+		}
+
+		conf := TestAccProvider.Meta().(Config)
+
+		// try to provision the virtual service
+		err := conf.ProvisionAResource("virtual_services", href)
+
+		if err != nil {
+			return fmt.Errorf("Provisioning failed for Virtual Service")
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckIllumioSBDataSource_VSDestroyed(s *terraform.State) error {
+	conf := TestAccProvider.Meta().(Config)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "illumio-core_virtual_service" {
+			continue
+		}
+
+		// try to provision the virtual service deletion
+		err := conf.ProvisionAResource("virtual_services", rs.Primary.ID)
+
+		if err != nil {
+			return fmt.Errorf("Teardown failed for Virtual Service")
+		}
+	}
+
+	return nil
 }
--- illumio-core/data_source_illumio_service_bindings_test.go
+++ illumio-core/data_source_illumio_service_bindings_test.go
@@ -4,6 +4,7 @@ package illumiocore
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -15,12 +16,19 @@ var prefixSBL string = "TF-ACC-SBL"
 func TestAccIllumioSBL_Read(t *testing.T) {
 	dataSourceName := "data.illumio-core_service_bindings.sbl_test"
 
+	virtualServiceName := acctest.RandomWithPrefix(prefixSB)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: TestAccProviderFactories,
+		CheckDestroy:      testAccCheckIllumioSBDataSource_VSDestroyed,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIllumioSBLDataSourceConfig_basic(),
+				Config: testAccCheckIllumioSBDataSourceConfig_VSSetup(virtualServiceName),
+				Check:  testAccCheckIllumioSBDataSource_VSExists("illumio-core_virtual_service.test"),
+			},
+			{
+				Config: testAccCheckIllumioSBLDataSourceConfig_basic(virtualServiceName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "items.#", "2"),
 				),
@@ -29,43 +37,33 @@ func TestAccIllumioSBL_Read(t *testing.T) {
 	})
 }
 
-func testAccCheckIllumioSBLDataSourceConfig_basic() string {
+func testAccCheckIllumioSBLDataSourceConfig_basic(virtualServiceName string) string {
 	rName1 := acctest.RandomWithPrefix(prefixSBL)
 	rName2 := acctest.RandomWithPrefix(prefixSBL)
-	rName3 := acctest.RandomWithPrefix(prefixSBL)
 
-	return fmt.Sprintf(`
-resource "illumio-core_virtual_service" "sbl_test" {
-	name = %[1]q
-	description = "Terraform Service Bindings test"
-	apply_to = "host_only"
+	var sb strings.Builder
 
-	service_ports {
-		proto = 6
-		port = 3389
-	}
-
-	service_ports {
-		proto = 17
-		port = 3389
-	}
+	sb.WriteString(testAccCheckIllumioSBDataSourceConfig_VSSetup(virtualServiceName))
+	sb.WriteString(fmt.Sprintf(`
+locals {
+	virtual_service_active_href = replace(illumio-core_virtual_service.test.href, "draft", "active")
 }
 
 resource "illumio-core_unmanaged_workload" "sbl_test1" {
-	name               = %[2]q
+	name               = %[1]q
 	description        = "Terraform Service Bindings test"
 	hostname           = "example.workload1"
 }
 
 resource "illumio-core_unmanaged_workload" "sbl_test2" {
-	name               = %[3]q
+	name               = %[2]q
 	description        = "Terraform Service Bindings test"
 	hostname           = "example.workload2"
 }
 
 resource "illumio-core_service_binding" "sbl_test1" {
 	virtual_service {
-		href = illumio-core_virtual_service.sbl_test.href
+		href = local.virtual_service_active_href
 	}
 	workload {
 		href = illumio-core_unmanaged_workload.sbl_test1.href
@@ -74,7 +72,7 @@ resource "illumio-core_service_binding" "sbl_test1" {
 
 resource "illumio-core_service_binding" "sbl_test2" {
 	virtual_service {
-		href = illumio-core_virtual_service.sbl_test.href
+		href = local.virtual_service_active_href
 	}
 	workload {
 		href = illumio-core_unmanaged_workload.sbl_test2.href
@@ -82,7 +80,7 @@ resource "illumio-core_service_binding" "sbl_test2" {
 }
 
 data "illumio-core_service_bindings" "sbl_test" {
-	virtual_service = illumio-core_virtual_service.sbl_test.href
+	virtual_service = local.virtual_service_active_href
 
 	# enforce dependencies
 	depends_on = [
@@ -90,5 +88,6 @@ data "illumio-core_service_bindings" "sbl_test" {
 		illumio-core_service_binding.sbl_test2,
 	]
 }
-`, rName1, rName2, rName3)
+`, rName1, rName2))
+	return sb.String()
 }

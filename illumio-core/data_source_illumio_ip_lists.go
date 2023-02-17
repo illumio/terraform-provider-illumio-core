9c8d5d4204df46c6630b00e6a8119c0252a42dab
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-02-16 20:58:38 -0500

    move readonly IP list schema into reusable function

--- illumio-core/data_source_illumio_ip_lists.go
+++ illumio-core/data_source_illumio_ip_lists.go
@@ -102,125 +102,136 @@ func datasourceIllumioIPLists() *schema.Resource {
 				Type:        schema.TypeList,
 				Computed:    true,
 				Description: "list of IP Lists",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"href": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "URI of this IP List",
-						},
-						"name": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Name of the IP List",
-						},
-						"description": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Description of the IP List",
-						},
-						"ip_ranges": {
-							Type:        schema.TypeList,
-							Computed:    true,
-							Description: "IP addresses or ranges",
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"description": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: "Description of the IP Range",
-									},
-									"from_ip": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: "IP address or a low end of IP range. Might be specified with CIDR notation",
-									},
-									"to_ip": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: "High end of an IP range",
-									},
-									"exclusion": {
-										Type:        schema.TypeBool,
-										Computed:    true,
-										Description: "Whether this IP address is an exclusion. Exclusions must be a strict subset of inclusive IP addresses",
-									},
-								},
-							},
-						},
-						"fqdns": {
-							Type:        schema.TypeList,
-							Computed:    true,
-							Description: "Collection of Fully Qualified Domain Names",
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"fqdn": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: "Fully Qualified Domain Name",
-									},
-									"description": {
-										Type:        schema.TypeString,
-										Computed:    true,
-										Description: "Description of FQDN",
-									},
-								},
-							},
-						},
-						"external_data_set": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "The data source from which a resource originates",
-						},
-						"external_data_reference": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "A unique identifier within the external data source",
-						},
-						"created_at": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Timestamp when this IP List was first created",
-						},
-						"updated_at": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Timestamp when this IP List was last updated",
-						},
-						"deleted_at": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Timestamp when this IP List was deleted",
-						},
-						"created_by": {
-							Type:        schema.TypeMap,
-							Computed:    true,
-							Description: "User who created this IP List",
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
-						"updated_by": {
-							Type:        schema.TypeMap,
-							Computed:    true,
-							Description: "User who last updated this IP List",
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
-						"deleted_by": {
-							Type:        schema.TypeMap,
-							Computed:    true,
-							Description: "User who deleted this IP List",
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
+				Elem:        ipListDataSourceSchema(false),
+			},
+		},
+	}
+}
+
+func ipListDataSourceSchema(hrefRequired bool) *schema.Resource {
+	s := map[string]*schema.Schema{
+		"name": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Name of the IP List",
+		},
+		"description": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Description of the IP List",
+		},
+		"ip_ranges": {
+			Type:        schema.TypeList,
+			Computed:    true,
+			Description: "IP addresses or ranges",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"description": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "Description of the IP Range",
+					},
+					"from_ip": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "IP address or a low end of IP range. Might be specified with CIDR notation",
+					},
+					"to_ip": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "High end of an IP range",
+					},
+					"exclusion": {
+						Type:        schema.TypeBool,
+						Computed:    true,
+						Description: "Whether this IP address is an exclusion. Exclusions must be a strict subset of inclusive IP addresses",
 					},
 				},
 			},
 		},
+		"fqdns": {
+			Type:        schema.TypeList,
+			Computed:    true,
+			Description: "Collection of Fully Qualified Domain Names",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"fqdn": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "Fully Qualified Domain Name",
+					},
+					"description": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "Description of FQDN",
+					},
+				},
+			},
+		},
+		"external_data_set": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "The data source from which a resource originates",
+		},
+		"external_data_reference": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "A unique identifier within the external data source",
+		},
+		"created_at": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Timestamp when this IP List was first created",
+		},
+		"updated_at": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Timestamp when this IP List was last updated",
+		},
+		"deleted_at": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Timestamp when this IP List was deleted",
+		},
+		"created_by": {
+			Type:        schema.TypeMap,
+			Computed:    true,
+			Description: "User who created this IP List",
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+		},
+		"updated_by": {
+			Type:        schema.TypeMap,
+			Computed:    true,
+			Description: "User who last updated this IP List",
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+		},
+		"deleted_by": {
+			Type:        schema.TypeMap,
+			Computed:    true,
+			Description: "User who deleted this IP List",
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+		},
 	}
+
+	hrefSchema := &schema.Schema{
+		Type:        schema.TypeString,
+		Description: "URI of this IP List",
+	}
+
+	if hrefRequired {
+		hrefSchema.Required = true
+	} else {
+		hrefSchema.Computed = true
+	}
+	s["href"] = hrefSchema
+
+	return &schema.Resource{Schema: s}
 }
 
 func datasourceIllumioIPListsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {

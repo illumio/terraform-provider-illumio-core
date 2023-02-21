6c43cfccfb805993c5d4fdcc2434e8b5b08dad15
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-02-21 16:14:28 -0500

    add omitempty to labels and assign_labels fields as they cannot both be present in the request

--- models/container_cluster_workload_profile.go
+++ models/container_cluster_workload_profile.go
@@ -11,8 +11,8 @@ type ContainerClusterWorkloadProfileLabel struct {
 type ContainerClusterWorkloadProfile struct {
 	Name            string                                 `json:"name"`
 	Description     string                                 `json:"description,omitempty"`
-	AssignLabels    []Href                                 `json:"assign_labels"`
-	Labels          []ContainerClusterWorkloadProfileLabel `json:"labels"`
+	AssignLabels    []Href                                 `json:"assign_labels,omitempty"`
+	Labels          []ContainerClusterWorkloadProfileLabel `json:"labels,omitempty"`
 	EnforcementMode string                                 `json:"enforcement_mode,omitempty"`
 	Managed         *bool                                  `json:"managed,omitempty"`
 }

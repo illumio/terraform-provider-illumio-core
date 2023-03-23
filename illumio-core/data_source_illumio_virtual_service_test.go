ff723a401a33dd43a230b03e4957adc747e14284
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-03-22 21:02:24 -0400

    fix virtual_service resource update and add acceptance tests

--- illumio-core/data_source_illumio_virtual_service_test.go
+++ illumio-core/data_source_illumio_virtual_service_test.go
@@ -16,12 +16,16 @@ func TestAccIllumioVS_Read(t *testing.T) {
 	dataSourceName := "data.illumio-core_virtual_service.vs_test"
 	resourceName := "illumio-core_virtual_service.vs_test"
 
+	vsName := acctest.RandomWithPrefix(prefixVS)
+	serviceName := acctest.RandomWithPrefix(prefixVS)
+	updatedName := acctest.RandomWithPrefix(prefixVS)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIllumioVSDataSourceConfig_basic(),
+				Config: testAccCheckIllumioVSDataSourceConfig_basic(vsName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair(dataSourceName, "href", resourceName, "href"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "name", resourceName, "name"),
@@ -33,6 +37,28 @@ func TestAccIllumioVS_Read(t *testing.T) {
 				),
 			},
 			{
+				Config: testAccCheckIllumioVSResource_updateRemoveAddresses(vsName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "ip_overrides.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "service_addresses.#", "0"),
+				),
+			},
+			{
+				Config: testAccCheckIllumioVSResource_updateToService(vsName, serviceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "apply_to", "internal_bridge_network"),
+					resource.TestCheckResourceAttr(resourceName, "service_ports.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "service.#", "1"),
+				),
+			},
+			{
+				Config: testAccCheckIllumioVSResource_updateNameAndDesc(updatedName, serviceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", updatedName),
+					resource.TestCheckResourceAttr(resourceName, "description", ""),
+				),
+			},
+			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
@@ -41,9 +67,20 @@ func TestAccIllumioVS_Read(t *testing.T) {
 	})
 }
 
-func testAccCheckIllumioVSDataSourceConfig_basic() string {
-	rName1 := acctest.RandomWithPrefix(prefixVS)
+func vsServiceConfig(serviceName string) string {
+	return fmt.Sprintf(`
+resource "illumio-core_service" "vs_https" {
+	name = %[1]q
 
+	service_ports {
+		proto = 6
+		port = 443
+	}
+}
+`, serviceName)
+}
+
+func testAccCheckIllumioVSDataSourceConfig_basic(vsName string) string {
 	return fmt.Sprintf(`
 resource "illumio-core_virtual_service" "vs_test" {
 	name = %[1]q
@@ -67,5 +104,48 @@ resource "illumio-core_virtual_service" "vs_test" {
 data "illumio-core_virtual_service" "vs_test" {
 	href = illumio-core_virtual_service.vs_test.href
 }
-`, rName1)
+`, vsName)
+}
+
+func testAccCheckIllumioVSResource_updateRemoveAddresses(vsName string) string {
+	return fmt.Sprintf(`
+resource "illumio-core_virtual_service" "vs_test" {
+	name = %[1]q
+	description = "Terraform Virtual Service test"
+	apply_to = "host_only"
+
+	service_ports {
+		proto = 6
+		port = 1234
+	}
+}
+`, vsName)
+}
+
+func testAccCheckIllumioVSResource_updateToService(vsName, serviceName string) string {
+	return vsServiceConfig(serviceName) + fmt.Sprintf(`
+resource "illumio-core_virtual_service" "vs_test" {
+	name = %[1]q
+	description = "Terraform Virtual Service test"
+	apply_to = "internal_bridge_network"
+
+	service {
+		href = illumio-core_service.vs_https.href
+	}
+}
+`, vsName)
+}
+
+func testAccCheckIllumioVSResource_updateNameAndDesc(updatedName, serviceName string) string {
+	return vsServiceConfig(serviceName) + fmt.Sprintf(`
+resource "illumio-core_virtual_service" "vs_test" {
+	name = %[1]q
+	description = ""
+	apply_to = "internal_bridge_network"
+
+	service {
+		href = illumio-core_service.vs_https.href
+	}
+}
+`, updatedName)
 }
--- models/virtual_service.go
+++ models/virtual_service.go
@@ -3,24 +3,24 @@
 package models
 
 type ServiceAddress struct {
-	IP          string `json:"ip,omitempty"`
-	Network     *Href  `json:"network,omitempty"`
-	Port        *int   `json:"port,omitempty"`
-	Fqdn        string `json:"fqdn,omitempty"`
-	Description string `json:"description,omitempty"`
+	IP          string  `json:"ip,omitempty"`
+	Network     *Href   `json:"network,omitempty"`
+	Port        *int    `json:"port,omitempty"`
+	Fqdn        string  `json:"fqdn,omitempty"`
+	Description *string `json:"description,omitempty"`
 }
 
 type VirtualService struct {
-	Name                  string           `json:"name"`
-	Description           string           `json:"description,omitempty"`
-	Labels                []Href           `json:"labels"`
-	ServicePorts          []ServicePort    `json:"service_ports,omitempty"`
-	Service               *Href            `json:"service,omitempty"`
-	IPOverrides           []string         `json:"ip_overrides"`
-	ApplyTo               string           `json:"apply_to,omitempty"`
-	ServiceAddresses      []ServiceAddress `json:"service_addresses"`
-	ExternalDataSet       string           `json:"external_data_set,omitempty"`
-	ExternalDataReference string           `json:"external_data_reference,omitempty"`
+	Name                  string            `json:"name,omitempty"`
+	Description           *string           `json:"description,omitempty"`
+	Labels                *[]Href           `json:"labels,omitempty"`
+	ServicePorts          *[]ServicePort    `json:"service_ports,omitempty"`
+	Service               *Href             `json:"service,omitempty"`
+	IPOverrides           *[]string         `json:"ip_overrides,omitempty"`
+	ApplyTo               string            `json:"apply_to,omitempty"`
+	ServiceAddresses      *[]ServiceAddress `json:"service_addresses,omitempty"`
+	ExternalDataSet       string            `json:"external_data_set,omitempty"`
+	ExternalDataReference string            `json:"external_data_reference,omitempty"`
 }
 
 // ToMap - Returns map for VirtualService model

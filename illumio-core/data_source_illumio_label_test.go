c84228edcab4fba273a3c62460b6527263078a07
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-05-30 10:29:48 -0400

    add label resource tests that verify recreate after remote deletion and no-op removal from state on destroy after remote deletion

--- illumio-core/data_source_illumio_label_test.go
+++ illumio-core/data_source_illumio_label_test.go
@@ -44,19 +44,63 @@ func TestAccIllumioLabel_Read(t *testing.T) {
 	})
 }
 
-func testAccCheckIllumioLabelDataSourceConfig_basic() string {
-	rName1 := acctest.RandomWithPrefix(prefixLabel)
+func TestAccIllumioLabelResource_Delete(t *testing.T) {
+	labelHref := new(string)
+	newLabelHref := new(string)
+	resourceName := "illumio-core_label.label_test"
+	labelValue := acctest.RandomWithPrefix(prefixLabel)
 
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIllumioLabelResourceConfig_basic(labelValue),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceExists(resourceName, labelHref),
+					resource.TestCheckResourceAttr(resourceName, "key", "app"),
+					resource.TestCheckResourceAttr(resourceName, "value", labelValue),
+				),
+			},
+			{
+				// check that an apply called after a label has been deleted
+				// correctly destroys and recreates the resource
+				PreConfig: deleteLabelFromPCE("app", labelValue, t),
+				Config:    testAccCheckIllumioLabelResourceConfig_basic(labelValue),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceExists(resourceName, newLabelHref),
+					testAccCheckCompareRefs(labelHref, newLabelHref, false),
+					resource.TestCheckResourceAttr(resourceName, "key", "app"),
+					resource.TestCheckResourceAttr(resourceName, "value", labelValue),
+				),
+			},
+			{
+				// check that a destroy called after a label has been deleted
+				// doesn't throw an error
+				PreConfig: deleteLabelFromPCE("app", labelValue, t),
+				Destroy:   true,
+				Config:    testAccCheckIllumioLabelResourceConfig_basic(labelValue),
+			},
+		},
+	})
+}
+
+func testAccCheckIllumioLabelResourceConfig_basic(value string) string {
 	return fmt.Sprintf(`
 resource "illumio-core_label" "label_test" {
 	key   = "app"
 	value = %[1]q
 }
+`, value)
+}
 
+func testAccCheckIllumioLabelDataSourceConfig_basic() string {
+	labelValue := acctest.RandomWithPrefix(prefixLabel)
+
+	return testAccCheckIllumioLabelResourceConfig_basic(labelValue) + `
 data "illumio-core_label" "label_test" {
 	href = illumio-core_label.label_test.href
-}
-`, rName1)
+}`
 }
 
 func testAccCheckIllumioLabeResource_updateValue(updatedValue string) string {
@@ -66,4 +110,29 @@ resource "illumio-core_label" "label_test" {
 	value = %[1]q
 }
 `, updatedValue)
+}
+
+// deleteLabelFromPCE removes the label with the given key/value
+// via the API to test Terraform drift behaviour
+func deleteLabelFromPCE(key, value string, t *testing.T) func() {
+	return func() {
+		conf := TestAccProvider.Meta().(Config)
+		illumioClient := conf.IllumioClient
+
+		endpoint := fmt.Sprintf("/orgs/%d/labels", illumioClient.OrgID)
+		_, data, err := illumioClient.Get(endpoint, &map[string]string{
+			"key":   key,
+			"value": value,
+		})
+		if err != nil {
+			t.Fatal("Failed to get label from PCE")
+		}
+
+		href := data.S("0", "href").Data().(string)
+
+		_, err = illumioClient.Delete(href)
+		if err != nil {
+			t.Fatal("Failed to delete label from PCE")
+		}
+	}
 }

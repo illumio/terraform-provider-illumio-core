66eccc5a8543178c5b26b2a9edde041a55fc4ff3
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-11-03 09:23:02 -0400

    add match_type param to label_types data source

--- illumio-core/data_source_illumio_label_types_test.go
+++ illumio-core/data_source_illumio_label_types_test.go
@@ -12,26 +12,40 @@ import (
 
 var prefixLTL string = "TF-ACC-LTL"
 
+func init() {
+	resource.AddTestSweepers("label_types", &resource.Sweeper{
+		Name: "label_types",
+		F:    sweep("label type", "display_name", prefixLTL, "/orgs/%d/label_dimensions"),
+	})
+}
+
 func TestAccIllumioLTL_Read(t *testing.T) {
 	dataSourceName := "data.illumio-core_label_types.ltl_test"
+	labelTypeName := acctest.RandomWithPrefix(prefixLTL)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIllumioLTLDataSourceConfig_basic(),
+				Config: testAccCheckIllumioLTLDataSourceConfig_basic(labelTypeName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "items.#", "2"),
+				),
+			},
+			{
+				Config: testAccCheckIllumioLTLDataSourceConfig_exactMatch(labelTypeName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "items.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "items.0.display_name", labelTypeName),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckIllumioLTLDataSourceConfig_basic() string {
-	rName1 := acctest.RandomWithPrefix(prefixLTL)
-	rName2 := acctest.RandomWithPrefix(prefixLTL)
+func ltlConfig(labelTypeName string) string {
+	rName := acctest.RandomWithPrefix(prefixLTL)
 
 	return fmt.Sprintf(`
 resource "illumio-core_label_type" "ltl_test1" {
@@ -43,10 +57,14 @@ resource "illumio-core_label_type" "ltl_test2" {
 	key          = %[2]q
 	display_name = %[2]q
 }
+`, rName, labelTypeName)
+}
 
+func testAccCheckIllumioLTLDataSourceConfig_basic(labelTypeName string) string {
+	return ltlConfig(labelTypeName) + fmt.Sprintf(`
 data "illumio-core_label_types" "ltl_test" {
 	# lookup based on partial match
-	display_name = %[3]q
+	display_name = %[1]q
 
 	# enforce dependencies
 	depends_on = [
@@ -54,5 +72,21 @@ data "illumio-core_label_types" "ltl_test" {
 		illumio-core_label_type.ltl_test2,
 	]
 }
-`, rName1, rName2, prefixLTL)
+`, prefixLTL)
+}
+
+func testAccCheckIllumioLTLDataSourceConfig_exactMatch(labelTypeName string) string {
+	return ltlConfig(labelTypeName) + fmt.Sprintf(`
+data "illumio-core_label_types" "ltl_test" {
+	# lookup using exact match
+	display_name = %[1]q
+	match_type   = "exact"
+
+	# enforce dependencies
+	depends_on = [
+		illumio-core_label_type.ltl_test1,
+		illumio-core_label_type.ltl_test2,
+	]
+}
+`, labelTypeName)
 }

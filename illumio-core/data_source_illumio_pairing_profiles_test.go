c2eeca892cdd185702d5c81c35dc687d9f5ecafd
Duncan Sommerville <duncan.sommerville@illumio.com>
2023-11-03 08:53:55 -0400

    add match_type param to pairing_profiles data source

--- illumio-core/data_source_illumio_pairing_profiles_test.go
+++ illumio-core/data_source_illumio_pairing_profiles_test.go
@@ -12,26 +12,40 @@ import (
 
 var prefixPPL string = "TF-ACC-PPL"
 
+func init() {
+	resource.AddTestSweepers("pairing_profiles", &resource.Sweeper{
+		Name: "pairing_profiles",
+		F:    sweep("pairing profile", "name", prefixPPL, "/orgs/%d/pairing_profiles"),
+	})
+}
+
 func TestAccIllumioPPL_Read(t *testing.T) {
 	dataSourceName := "data.illumio-core_pairing_profiles.ppl_test"
+	profileName := acctest.RandomWithPrefix(prefixPPL)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIllumioPPLDataSourceConfig_basic(),
+				Config: testAccCheckIllumioPPLDataSourceConfig_basic(profileName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(dataSourceName, "items.#", "2"),
+				),
+			},
+			{
+				Config: testAccCheckIllumioPPLDataSourceConfig_exactMatch(profileName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "items.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "items.0.name", profileName),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckIllumioPPLDataSourceConfig_basic() string {
-	rName1 := acctest.RandomWithPrefix(prefixPPL)
-	rName2 := acctest.RandomWithPrefix(prefixPPL)
+func pplConfig(profileName string) string {
+	rName := acctest.RandomWithPrefix(prefixPPL)
 
 	return fmt.Sprintf(`
 resource "illumio-core_pairing_profile" "ppl_test1" {
@@ -65,10 +79,14 @@ resource "illumio-core_pairing_profile" "ppl_test2" {
 	visibility_level_lock = false
 	enforcement_mode      = "visibility_only"
 }
+`, rName, profileName)
+}
 
+func testAccCheckIllumioPPLDataSourceConfig_basic(profileName string) string {
+	return pplConfig(profileName) + fmt.Sprintf(`
 data "illumio-core_pairing_profiles" "ppl_test" {
 	# lookup based on partial match
-	name = %[3]q
+	name = %[1]q
 
 	# enforce dependencies
 	depends_on = [
@@ -76,5 +94,21 @@ data "illumio-core_pairing_profiles" "ppl_test" {
 		illumio-core_pairing_profile.ppl_test2,
 	]
 }
-`, rName1, rName2, prefixPPL)
+`, prefixPPL)
+}
+
+func testAccCheckIllumioPPLDataSourceConfig_exactMatch(profileName string) string {
+	return pplConfig(profileName) + fmt.Sprintf(`
+data "illumio-core_pairing_profiles" "ppl_test" {
+	# lookup using exact match
+	name       = %[1]q
+	match_type = "exact"
+
+	# enforce dependencies
+	depends_on = [
+		illumio-core_pairing_profile.ppl_test1,
+		illumio-core_pairing_profile.ppl_test2,
+	]
+}
+`, profileName)
 }
